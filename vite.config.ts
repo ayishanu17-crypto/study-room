366aa9a78cddce33678dd2aef570533291483472
SHAIK AYISHA <ayishanu17@gmail.com>
2026-07-24 13:55:57 +0530

    Regenerate lock file and update config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/study-room/', // Replace 'your-repo-name' with your actual GitHub repository name
   plugins: [
     react(),
     tailwindcss(),
