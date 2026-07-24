b1376c2c81c8b235e8e235b6d90253bd629c6ef5
SHAIK AYISHA <ayishanu17@gmail.com>
2026-07-24 14:06:53 +0530

    Change Vite base path to root for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/study-room/', // Replace 'your-repo-name' with your actual GitHub repository name
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
